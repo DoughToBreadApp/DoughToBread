c73fd60ed4baf75e5821effdac0dfe6b63254aa4
Alexander Rowshan <arowshan@usc.edu>
2024-09-13 16:43:59 -0700

    First

---
+++ DoughToBread/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoughToBread
+//
+//  Created by Alex Rowshan on 9/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Dough To Bread!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoughToBread/DoughToBreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoughToBreadApp.swift
+//  DoughToBread
+//
+//  Created by Alex Rowshan on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DoughToBreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
