9ba3eafb16913374e4d228d5614f24a39553b4f6
kevingerges <kevingerges47@gmail.com>
2024-10-19 19:04:14 -0700

    broooo

---
+++ DoughToBread/Model/Module.swift
@@ -0,0 +1,14 @@
+//
+//  Module.swift
+//  DoughToBread
+//
+//  Created by Kevin Gerges on 10/19/24.
+//
+
+import Foundation
+
+struct Module: Identifiable {
+    var id: String
+    var name: String
+    var description: String
+}
