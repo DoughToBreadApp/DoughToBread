b9de77de12b26f1bd84e457ffabd2f0de2723f85
Neil Maharishi <maharish@usc.edu>
2024-10-29 17:00:25 -0700

    Fixed compile bug

---
+++ DoughToBread/ViewModel/Badge.swift
@@ -0,0 +1,34 @@
+//
+//  Badge.swift
+//  DoughToBread
+//
+//  Created by Neil Maharishi on 10/29/24.
+//
+
+
+//
+//  Badge.swift
+//  DoughToBread
+//
+//  Created by Neil Maharishi on 10/21/24.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Badge: Identifiable, Codable {
+    var id: String
+    var name: String
+    var description: String
+    var level: BadgeLevel
+    var type: BadgeType
+    var dateEarned: Date
+    
+    enum BadgeLevel: String, Codable {
+        case beginner, intermediate, advanced, expert
+    }
+    
+    enum BadgeType: String, Codable {
+        case knowledge, skill, achievement, community
+    }
+}
